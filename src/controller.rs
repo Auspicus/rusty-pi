73f7ed94748aec7850e335190473fb38c1049563
David Disch <elijahdisch@gmail.com>
2021-11-09 21:54:40 +1000

    Throw an Arc<Mutex> at it?

--- src/controller.rs
+++ src/controller.rs
@@ -1,11 +1,11 @@
+use rppal::gpio::OutputPin;
 use std::thread;
 use std::time::Duration;
-use rppal::gpio::{OutputPin};
 
 pub const ENGINE_PIN: u8 = 23;
 
 pub fn flip_flop_pin(pin: &mut OutputPin, secs: u64) {
-  pin.set_high();
-  thread::sleep(Duration::from_secs(secs));
-  pin.set_low();
+    pin.set_high();
+    thread::sleep(Duration::from_secs(secs));
+    pin.set_low();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,49 @@
-use std::net::TcpListener;
-use std::thread::spawn;
+use rppal::gpio::{Gpio, OutputPin};
+use std::sync::Arc;
+use std::{net::TcpListener, sync::Mutex};
+use std::thread;
 use tungstenite::accept;
-use rppal::gpio::{Gpio};
 
-use rusty_pi::controller::{ENGINE_PIN, flip_flop_pin};
+use rusty_pi::controller::{flip_flop_pin, ENGINE_PIN};
 
-const BIND_ADDRESS: &'static str =  "0.0.0.0:9001";
+const BIND_ADDRESS: &'static str = "0.0.0.0:9001";
 
-fn main () {
+fn main() {
     let server = TcpListener::bind(BIND_ADDRESS).unwrap();
     println!("Started websocket server @ {}", BIND_ADDRESS);
 
-    let gpio = Gpio::new()
-        .expect("Expected an accessible GPIO device but found none.");
-    let mut engine_pin = gpio.get(ENGINE_PIN)
+    let gpio = Gpio::new().expect("Expected an accessible GPIO device but found none.");
+    let engine_pin = gpio
+        .get(ENGINE_PIN)
         .expect("Expected pin to be accessible but it was not.")
         .into_output();
     println!("Connected to on-board GPIO");
 
+    let pin_arc: Arc<Mutex<OutputPin>> = Arc::new(Mutex::new(engine_pin));
+
     // This is really stupid.
     // Should be handling connections using threads.
-    'connectionLoop: for stream in server.incoming() {
+    for stream in server.incoming() {
         let mut websocket = accept(stream.unwrap()).unwrap();
+        let pin_arc_copy: Arc<Mutex<OutputPin>> = Arc::clone(&pin_arc);
 
-        loop {
-            let msg = match websocket.read_message() {
-                Ok(msg) => msg,
-                Err(_) => continue 'connectionLoop,
-            };
+        thread::spawn(move || {
+            loop {
+                let msg = match websocket.read_message() {
+                    Ok(msg) => msg,
+                    Err(_) => break,
+                };
 
-            println!("Received: {:?}", msg);
+                println!("Received: {:?}", msg);
 
-            // We do not want to send back ping/pong messages.
-            if msg.is_binary() || msg.is_text() {
-                flip_flop_pin(&mut engine_pin, 1);
-                println!("Responded: {}", msg);
-                websocket.write_message(msg).unwrap();
+                // We do not want to send back ping/pong messages.
+                if msg.is_binary() || msg.is_text() {
+                    let mut pin = pin_arc_copy.lock().expect("Mutex poisoned by another thread.");
+                    flip_flop_pin(&mut pin, 1);
+                    println!("Responded: {}", msg);
+                    websocket.write_message(msg).unwrap();
+                }
             }
-        }
+        });
     }
 }
